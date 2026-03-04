eb2a9a37c5938454a0089fbf8e497dd52eae07c8
Heri23F <heri.github.com@gmail.com>
2026-03-05 02:28:38 +0700

    Create this following function: 1. getRandomInt randomly return number 2. getComputerChoiche randomly return rock, paper, scissors 3. getHumanChoiche get user input and return it

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 console.log("Hello World")
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max)
+}
+
+function getComputerChoiche() {
+    let choiche
+    let random = getRandomInt(3)
+
+    if (random === 1) {
+        choiche = "rock"
+    } else if (random === 2) {
+        choiche = "paper"
+    } else {
+        choiche = "scissor"
+    }
+    return choiche
+    console.log(random)
+
+    
+}
+
+console.log(getComputerChoiche())
+
